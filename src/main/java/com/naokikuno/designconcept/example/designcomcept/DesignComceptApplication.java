be0fdbe0f0e51e828945abea311e9c96778c5f1e
naokikuno <yuushiranui@gmail.com>
2023-09-24 15:37:15 +0900

    initial commit

---
+++ src/main/java/com/naokikuno/designconcept/example/designcomcept/DesignComceptApplication.java
@@ -0,0 +1,13 @@
+package com.naokikuno.designconcept.example.designcomcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignComceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignComceptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naokikuno/designconcept/example/designcomcept/DesignComceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naokikuno.designconcept.example.designcomcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignComceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
